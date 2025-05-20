ea1a81f529c39f1e981e4252cf240378b04f21fe
Jackson Barnes <jackson.barnes162@gmail.com>
2025-05-20 19:15:31 +0100

    Init

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Jackson Barnes on 20/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
